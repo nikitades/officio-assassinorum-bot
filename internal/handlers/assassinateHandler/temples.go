f1d43d69fafaad3f5b2798843c039ce055ca86ee
Nikita Pavlovskiy <nikita.pavlovskiy@fromjimmy.com>
2022-08-18 21:12:35 +0300

    Lower the chances

--- internal/handlers/assassinateHandler/temples.go
+++ internal/handlers/assassinateHandler/temples.go
@@ -24,37 +24,37 @@ var temples = []temple{
 	temple{
 		name:   "Vindicare",
 		image:  "https://static.wikia.nocookie.net/warhammer40k/images/1/14/Vindicare_Temple_Icon.jpg",
-		chance: 0.04,
+		chance: 0.004,
 	},
 	temple{
 		name:   "Callidus",
 		image:  "https://static.wikia.nocookie.net/warhammer40k/images/1/14/Callidus_Temple_Icon.jpg",
-		chance: 0.03,
+		chance: 0.003,
 	},
 	temple{
 		name:   "Eversor",
 		image:  "https://static.wikia.nocookie.net/warhammer40k/images/0/01/Eversor_Temple_Icon.jpg",
-		chance: 0.11,
+		chance: 0.011,
 	},
 	temple{
 		name:   "Culexus",
 		image:  "https://static.wikia.nocookie.net/warhammer40k/images/3/31/Cullexus_Temple_Icon.jpg",
-		chance: 0.02,
+		chance: 0.002,
 	},
 	temple{
 		name:   "Venenum",
 		image:  "https://static.wikia.nocookie.net/warhammerfb/images/8/87/Poison.jpg",
-		chance: 0.02,
+		chance: 0.002,
 	},
 	temple{
 		name:   "Vanus",
 		image:  "https://yt3.ggpht.com/ytc/AKedOLQyXRhiC3MHRO2q_o3TT151tQnfNGSESfGxf8w8_w=s900-c-k-c0x00ffffff-no-rj",
-		chance: 0.03,
+		chance: 0.003,
 	},
 	temple{
 		name:   "Maerorus",
 		image:  "https://i.pinimg.com/736x/cf/2f/c3/cf2fc30aec60c58441e0f45061e1c457.jpg",
-		chance: 0.001,
+		chance: 0.0001,
 	},
 }
 
