86d0912d7ed92930da1997aab7953d8d00570a06
Nikita Pavlovskiy <nikita.pavlovskiy@fromjimmy.com>
2022-08-22 16:52:49 +0300

    Add username to merciful response

--- internal/handlers/eliminateSpecialTarget/eliminateSpecialTarget.go
+++ internal/handlers/eliminateSpecialTarget/eliminateSpecialTarget.go
@@ -41,7 +41,7 @@ func Assassinate(update tgbotapi.Update) {
 }
 
 func doNotAssassinate(username string) string {
-	return fmt.Sprintf("_@%v_ was judged by the secret court and found not guilty!")
+	return fmt.Sprintf("_@%v_ was judged by the secret court and found not guilty!", username)
 }
 
 func doAssassinate(chatId, targetId int, username string, temple temples.Temple) (string, string) {
